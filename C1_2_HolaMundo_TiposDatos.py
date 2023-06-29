6a211e52c90c2d3b2df9927d4dc9dc3c614920a3
oscar diaz <osca.diazs@duocuc.cl>
2023-06-29 18:07:27 -0400

    Avances clases 1,2 y 3

---
+++ C1_2_HolaMundo_TiposDatos.py
@@ -0,0 +1,12 @@
+# Este es un comentario con hashtag
+"""
+Este es un comentario entre 3 comillas doble
+"""
+'''
+Este es un comentario entre 3 comillas simple
+'''
+print("Hola mundo") #Print imprime en consola.
+print(type("Hola mundo")) #Type sirve para consultar el tipo de dato.
+print(type(2)) #tipo de dato numerico (int)
+print(type(2.5)) #tipo de dato float
+print(type(True)) #tipo de dato boolean
---
+++ C3_Variables.py
@@ -0,0 +1,20 @@
+#Variables
+
+#Variable String
+my_variable = "My string Variable"
+print(my_variable)
+
+#Variable Int
+my_int_variable = 5
+print(my_int_variable)
+
+#Variable bool
+my_bool_variable = True
+print(my_bool_variable)
+
+#Imprimir variables hechas concatenadas
+print(my_variable, my_int_variable, my_bool_variable)
+
+#Parseo a string de un int
+variable_int = 10
+print(str(variable_int))
